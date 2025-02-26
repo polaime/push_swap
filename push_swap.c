3d2fc7ee2013700c53f73967c89709bd2f48d43d
poul <paulthibaud909@gmail.com>
2025-02-26 19:38:38 +0100

    Premier algo fonctionnel mais guez de fou

--- push_swap.c
+++ push_swap.c
@@ -2,57 +2,66 @@
 // turc mecanique
 int verif_tri(t_stack *stack_a)
 {
-    while (stack_a && stack_a -> next)
+	t_stack *current = stack_a;
+    while (current && current -> next)
     {
-        if (stack_a -> value > stack_a -> next -> value)
+        if (current -> value > current -> next -> value)
             return (0);
-        stack_a = stack_a -> next;
+        current = current -> next;
     }
     return (1);
 }
-int calcul_mediane (t_stack *stack_a, int i)
-{
-	int	 mediane;
-	int	 j;
+// int calcul_mediane (t_stack *stack_a, int i)
+// {
+// 	int	 mediane;
+// 	int	 j;
 
-	j = 0;	
-	mediane = 0;
-	t_stack *mid = stack_a;
-	while (j < i / 2)
-	{
-		mid = mid -> next;
-		j++;
-	}
-	mediane = mid -> value;
-	printf("%i\n", mediane);
-	return (mediane);
-}
+// 	j = 0;	
+// 	mediane = 0;
+// 	t_stack *mid = stack_a;
+// 	while (j < i / 2)
+// 	{
+// 		mid = mid -> next;
+// 		j++;
+// 	}
+// 	mediane = mid -> value;
+// 	printf("%i\n", mediane);
+// 	return (mediane);
+// }
 t_stack *utils_algo(t_stack *stack_a, int i)
-{
-	int		j;
-	int		mediane;
-	t_stack *stack_b;
-	
+{    
+	int j;
+    t_stack *stack_b;
+	int	k;
+
+	k = 0;
 	stack_b = NULL;
-	j = 0;
-	while (!verif_tri(stack_a))
+    while (!verif_tri(stack_a))
 	{
-			mediane = calcul_mediane(stack_a, i);
-			j = 0;
-			while (stack_a != NULL && j != i)
+		t_stack *current = stack_a;
+        t_stack *noeud_suivant;
+
+        while (current != NULL && j < i) {
+            noeud_suivant = current->next;
+
+            if (noeud_suivant != NULL) 
 			{
-				if (stack_a -> value < mediane)
-					push_b(&stack_a, &stack_b);
-				else
-					rotate_a(&stack_a);
-				j++;
-			}
-		while (stack_b != NULL)
-			push_a(&stack_a, &stack_b);
-		print_stack(stack_a);
-	}
-	return (stack_a);
+                if (current->value < noeud_suivant->value)
+                    k = k + push_b(&stack_a, &stack_b);
+                else 
+                    k = k + swap_a(&stack_a);
+            }
+            current = noeud_suivant;
+            j++;
+        }
+        while (stack_b != NULL)
+            push_a(&stack_a, &stack_b);
+        print_stack(stack_a);
+    }
+	printf("%i\n", k);
+    return stack_a;
 }
+
 t_stack *push_swap(t_stack **stack_a, int argc)
 {
 	int	 i;
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -1,35 +1,38 @@
 #include "push_swap.h"
 
-void push_b(t_stack **stack_a, t_stack **stack_b)
+int push_b(t_stack **stack_a, t_stack **stack_b)
 {
 	if (*stack_a == NULL)
-		return; 
+		return (0); 
 	t_stack *temp = *stack_a;
 	*stack_a = (*stack_a)-> next;
 	temp -> next = *stack_b;
 	*stack_b = temp;
 	ft_putstr("pb\n");
+	return (1);
 }
-void push_a(t_stack **stack_a, t_stack **stack_b)
+int push_a(t_stack **stack_a, t_stack **stack_b)
 {
 	if (*stack_b == NULL)
-		return; 
+		return (0); 
 	t_stack *temp = *stack_b;
 	*stack_b = (*stack_b)-> next;
 	temp -> next = *stack_a;
 	*stack_a = temp;
 	ft_putstr("pa\n"); 
+	return (1);
 }
-void swap_a(t_stack **stack_a)
+int swap_a(t_stack **stack_a)
 {
 	if (*stack_a == NULL || (*stack_a) -> next == NULL)
-		return; 
+		return (0); 
 	t_stack *tete = (*stack_a);
 	t_stack *cou = tete -> next;
 	tete -> next = cou -> next;
 	cou -> next = tete;
 	*stack_a = cou;
-	ft_putstr("sa\n"); 
+	ft_putstr("sa\n");
+	return (1);
 }
 void swap_b(t_stack **stack_b)
 {
